23feb56d484a78c48c318354b8697b6303f86a26
liaolintao <1413920792@qq.com>
2020-08-20 18:13:46 +0800

    fix: update hugo 1.2.3, update aspectj, fix ClassFormatException caused by File: 'module-info.class'

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -26,10 +26,10 @@ class HugoPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.2-SNAPSHOT'
+      debugCompile 'com.jakewharton.hugo.fix:hugo-runtime:1.2.3'
       // TODO this should come transitively
-      debugCompile 'org.aspectj:aspectjrt:1.8.6'
-      compile 'com.jakewharton.hugo:hugo-annotations:1.2.2-SNAPSHOT'
+      debugCompile 'org.aspectj:aspectjrt:1.9.6'
+      compile 'com.jakewharton.hugo.fix:hugo-annotations:1.2.3'
     }
 
     project.extensions.create('hugo', HugoExtension)
